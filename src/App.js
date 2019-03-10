10f24b5493c78749bb4bd333aae2fcd591390742
maykl4iv <mishaiv1995@gmail.com>
2019-03-10 15:02:05 +0100

    connect to server and add some feature

--- src/App.js
+++ src/App.js
@@ -34,8 +34,27 @@ class App extends Component {
       imageUrl : '',
       box : {},
       route : 'signin',
-      isSignedIn : false
+      isSignedIn : false,
+      user: {
+        id: '',
+        name: '',
+        password: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState( {user : {
+      id: data.id,
+      name: data.name,
+      password: data.password,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -63,7 +82,22 @@ class App extends Component {
     this.setState({ imageUrl : this.state.input })
     app.models
       .predict( Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response => this.displayFaceBox( this.calculateFaceLocation(response) )  )
+      .then(response => {
+        if(response){
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(resp => resp.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }))
+            })
+        }
+        this.displayFaceBox( this.calculateFaceLocation(response) )
+      })
       .catch( error => console.log(error))
   }
 
@@ -88,7 +122,7 @@ class App extends Component {
         { route === 'home'
           ? <div>
             <Logo />
-            <Rank />
+            <Rank name={this.state.user.name} entries={this.state.user.entries} />
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
@@ -97,8 +131,8 @@ class App extends Component {
           </div>
           : (
               route === 'signin'
-              ?<Signin onRouteChange={this.onRouteChange}/>
-              :<Register onRouteChange={this.onRouteChange}/>
+              ?<Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+              :<Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
             )
         }
       </div>
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Rank = () => {
+const Rank = ({ name, entries }) => {
   return (
     <div>
       <div className='white f3'>
-        {'Misha, your curent rank is...'}
+        {`${name}, your curent rank is...`}
       </div>
       <div className='white f1'>
-        {'#1'}
+        {entries}
       </div>
     </div>
   )
