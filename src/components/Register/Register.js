2b81672f533e3249ceed752118085cbc0ab85d9b
maykl4iv <mishaiv1995@gmail.com>
2019-03-21 13:44:07 +0100

    Add verification email

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+const validateEmail = (email) => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 class Register extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       email: '',
       password: '',
-      name: ''
+      name: '',
+      error: ''
     }
   }
 
@@ -22,23 +28,28 @@ class Register extends React.Component {
     this.setState({ password: event.target.value });
   }
 
-  onSubmitSignIn = () => {
-    fetch('https://gentle-shelf-15430.herokuapp.com/register', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        name: this.state.name,
-        email: this.state.email,
-        password: this.state.password
+  onSubmitRegister = () => {
+    if( !validateEmail(this.state.email) ){
+      return this.setState({error: 'Unvalidated email'})
+    }else{
+      this.setState({error: ''})
+      fetch('https://gentle-shelf-15430.herokuapp.com/register', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          name: this.state.name,
+          email: this.state.email,
+          password: this.state.password
+        })
       })
-    })
-      .then( resp => resp.json())
-      .then( user => {
-        if( user.id ){
-          this.props.loadUser(user);
-          this.props.onRouteChange('home');
-        }
-      })
+        .then( resp => resp.json())
+        .then( user => {
+          if( user.id ){
+            this.props.loadUser(user);
+            this.props.onRouteChange('home');
+          }
+        })
+    }
   }
 
   render(){
@@ -46,6 +57,7 @@ class Register extends React.Component {
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5  center">
         <main className="pa4 black-80">
           <form className="measure">
+
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Register</legend>
               <div className="mt3">
@@ -56,6 +68,7 @@ class Register extends React.Component {
                 name="name"
                 id="name"
                 onChange={this.onNameChange}
+                required
                 />
               </div>
               <div className="mt3">
@@ -66,8 +79,10 @@ class Register extends React.Component {
                 name="email-address"
                 id="email-address"
                 onChange={this.onEmailChange}
+                required
                 />
               </div>
+              {this.state.error ? <h3>You haven't valid email</h3> : true }
               <div className="mv3">
                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                 <input
@@ -76,12 +91,13 @@ class Register extends React.Component {
                 name="password"
                 id="password"
                 onChange={this.onPasswordChange}
+                required
                 />
               </div>
             </fieldset>
             <div className="">
               <input
-                onClick={this.onSubmitSignIn}
+                onClick={this.onSubmitRegister}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="button"
                 value="Register"
